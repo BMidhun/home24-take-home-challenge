72a66ef95bddaa84e918792a38cfdd33a55faba0
b_midhun <midhunbabygeorge@gmail.com>
2022-05-11 13:18:48 +0530

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -13,6 +13,4 @@ if (process.env.NODE_ENV === "development") {
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-    <App />
-);
+root.render(<App />);
